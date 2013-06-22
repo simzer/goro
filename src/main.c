cef7dbfb50dbed810f63a2c1907f6f26f2d563b2
Laszlo Simon <laszlo.simon@gmail.com>
2013-06-22 19:00:25 +0200

    Minimal tests added.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "board.h"
 #include "game.h"
@@ -9,10 +10,15 @@
 #include "ui.h"
 #include "ai.h"
 
-int main(void)
+int main(int argc, char *argv[])
 {
-  //Game game = tictactoe_create(3);
-  Game game = gomoko_create(board_create(9,9));
+  Game game;
+  if ((argc > 1) && (strcmp(argv[1], "--tictactoe") == 0)) {
+    game = tictactoe_create(3);
+  } else {
+    game = gomoko_create(board_create(9,9));
+  }
+  MiniMax miniMax = miniMax_create(&game);
   Player winner;
   while(winner = game.winner(&game),
         (   winner == player_none
@@ -21,8 +27,12 @@ int main(void)
     BoardCoord coord;
     game_switchPlayer(&game);
     board_print(&game.board);
-    coord = (game.actPlayer == player_1) ? ui_move(&game) : ai_move(&game);
+    coord = (game.actPlayer == player_1)
+            ? ui_move(&game)
+            : miniMax_move(&miniMax);
     board_setCell(&game.board, coord, game_actPlayerCell(&game));
+    printf("Player %d step: %c%d\n",
+           game.actPlayer, 'a'+coord.col, 1+coord.row);
   }
   board_print(&game.board);
   printf(winner == player_none
---
+++ test/test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+function test {
+  echo $2 | bin/goro $1 | grep -q $3;
+  echo $1: $2 "->" $3
+  if [ $? -ne 0 ]; then
+    echo "                                                              failed" 
+  else
+    echo "                                                              passed"
+  fi
+}
+
+make all
+test "--tictactoe" "a1\na2\nc1\nb3\nc3\n" "Nobody"
+test "--tictactoe" "a1\na2\nb3\n" "\"Player 1\""
