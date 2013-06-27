0e274d861dcbec60d8fd4aa9d5918843bbb2fe47
Laszlo.simon <laszlo.simon@gmail.com>
2013-06-28 00:19:38 +0200

    Empty go game class added.

---
+++ src/go.c
@@ -0,0 +1,60 @@
+/* This file is part of Goro. Goro is licensed under the terms of the
+   GNU General Public License version 3. See <http://www.gnu.org/licenses/>.
+
+   Copyright (C) 2013 Goro Team <https://github.com/goro-dev?tab=members> */
+
+#include <string.h>
+
+#include "game.h"
+#include "go.h"
+#include "boarditerator.h"
+
+static int validGoMove(Game *self, BoardCoord coord);
+static int possibleGoMoves(Game *self);
+static Player goWinner(Game *self);
+static double evalGoPosition(Game *self);
+
+static const GameVirtualTable goVirtualtable = {
+  &validGoMove,
+  &possibleGoMoves,
+  &goWinner,
+  &evalGoPosition
+};
+
+/*
+switchGamePlayer:
+  player moves
+  remove every group of other player without empty neighbour
+  remove every white group without empty neighbour
+*/
+
+Game createGo(Board board)
+{
+  // to be implemented
+}
+
+static int validGoMove(Game *self, BoardCoord coord)
+{
+  // to be implemented
+  /*
+    move is on empty cell
+    move not dies instantly
+    not results the same position as the one before previous
+  */
+}
+
+static int possibleGoMoves(Game *self)
+{
+  // to be implemented
+}
+
+static Player goWinner(Game *self)
+{
+  // to be implemented
+}
+
+static double evalGoPosition(Game *self)
+{
+  // to be implemented
+}
+
---
+++ src/go.h
@@ -0,0 +1,13 @@
+/* This file is part of Goro. Goro is licensed under the terms of the
+   GNU General Public License version 3. See <http://www.gnu.org/licenses/>.
+
+   Copyright (C) 2013 Goro Team <https://github.com/goro-dev?tab=members> */
+
+#ifndef __GO_H__
+#define __GO_H__
+
+#include "game.h"
+
+extern Game createGo(Board board);
+
+#endif
