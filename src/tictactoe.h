ad2bb47330feb42e425cd182c18b1bf3bfb1148b
Laszlo.simon <laszlo.simon@gmail.com>
2013-06-27 21:38:26 +0200

    Copyright and licence added to source files.

--- src/tictactoe.h
+++ src/tictactoe.h
@@ -1,3 +1,7 @@
+/* This file is part of Goro. Goro is licensed under the terms of the
+   GNU General Public License version 3. See <http://www.gnu.org/licenses/>.
+
+   Copyright (C) 2013 Goro Team <https://github.com/goro-dev?tab=members> */
 
 #ifndef __TICTACTOE_H__
 #define __TICTACTOE_H__
