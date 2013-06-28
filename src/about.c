370712a91a1d307a7572d24dfb7478602db60fe5
Laszlo.simon <laszlo.simon@gmail.com>
2013-06-29 01:55:55 +0200

    About unit added.

---
+++ src/about.c
@@ -0,0 +1,9 @@
+/* This file is part of Goro. Goro is licensed under the terms of the
+   GNU General Public License version 3. See <http://www.gnu.org/licenses/>.
+
+   Copyright (C) 2013 Goro Team <https://github.com/goro-dev?tab=members> */
+
+#include "about.h"
+
+const char programName[] = "Goro";
+const char programVersion[] = "0.1.0";
---
+++ src/about.h
@@ -0,0 +1,12 @@
+/* This file is part of Goro. Goro is licensed under the terms of the
+   GNU General Public License version 3. See <http://www.gnu.org/licenses/>.
+
+   Copyright (C) 2013 Goro Team <https://github.com/goro-dev?tab=members> */
+
+#ifndef __ABOUT_H__
+#define __ABOUT_H__
+
+extern const char programName[];
+extern const char programVersion[];
+
+#endif
